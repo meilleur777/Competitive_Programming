85e88fac08bfb0d49f7496878accd5b3d02fa340
손태호 <sarcasm1387@gmail.com>
2024-05-05 16:01:34 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 4.11 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(vector<vector<string>> v) {
+    
+    map<string, int> mp;
+    for (auto c:v) {
+        mp[c[1]]++;
+    }
+    int ret=1;
+    for (auto [p, q]:mp) {
+        ret*=(q+1);
+    }
+
+    return ret-1;
+}
