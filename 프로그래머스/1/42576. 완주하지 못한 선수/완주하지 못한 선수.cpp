2816c82ea74369f224e29e158d9e62a0288e7f8c
손태호 <sarcasm1387@gmail.com>
2024-05-05 16:02:32 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 105.70 ms, Memory: 32.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string solution(vector<string> par, vector<string> com) {
+    map<string, int> mp;
+    for (auto s:com) {
+        mp[s]++;
+    }
+    for (auto s:par) {
+        mp[s]--;
+    }
+    for (auto [p, q]:mp) {
+        if (q!=0) return p;
+    }
+}
