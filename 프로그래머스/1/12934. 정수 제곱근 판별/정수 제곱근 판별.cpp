bd1a47237784e7caa1049c4d7b36c34801856a68
손태호 <sarcasm1387@gmail.com>
2024-05-15 21:35:35 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long solution(long long n) {
+    for (long long i=1; i*i<=n; i++) {
+        if (i*i==n) return (i+1)*(i+1);
+    }
+    return -1;
+}
